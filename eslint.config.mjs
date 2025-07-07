bd241aec8309e42e3d8c777ab0d84436cdea4175
David Evans <devans@darkrock.dev>
2025-07-07 09:51:07 +0100

    Install ESLint and Prettier. Configure for single quotes, trailing commas, add format script

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,13 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    files: ['**/*.{ts,tsx}'],
+    rules: {
+      semi: ['error', 'never'],
+    },
+  },
+  ...compat.extends('prettier'),
 ];
 
 export default eslintConfig;
